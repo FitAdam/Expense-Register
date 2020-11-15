514663f4874169979e07e0ad7c04f4abba73ad33
FitAdam <adam.george617@gmail.com>
2020-11-15 23:18:57 +0000

    types of payments and currencies

--- expense_register/settings.py
+++ expense_register/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from secret_settings import MY_SECRET_KEY, MY_DB_PASSWORD
+#from secret_settings import MY_SECRET_KEY, MY_DB_PASSWORD
 import os
 from pathlib import Path
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = MY_SECRET_KEY
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ExpensesDemo',
         'USER': 'postgres',
-        'PASSWORD': MY_DB_PASSWORD,
+        'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
