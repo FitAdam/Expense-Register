87d0fcf00697ae2d840ad9eee09a423ea8f98cc4
FitAdam <adam.george617@gmail.com>
2020-11-15 23:18:08 +0000

    types of payments and currencies

--- expenses_app/forms.py
+++ expenses_app/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from .models import Expense
 
+
+
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
--- expenses_app/models.py
+++ expenses_app/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+CURRENCY_CHOICES = [('£','£'),('$','$'),('€','€')]
+CURRENCY_TYPE = [('Cash', 'cash'), ('Card', 'card')]
+
 class Expense(models.Model):
     """A basic expense"""
     date_added = models.DateTimeField(auto_now_add=True)
-    type_used = models.CharField(max_length=30)
+    type_used = models.CharField(max_length=32, choices=CURRENCY_TYPE, default='card')
     company_name = models.CharField(max_length=100)
     amount = models.FloatField()
-    currency = models.CharField(default='€', max_length=2)
+    currency = models.CharField(default='€', max_length=16, choices=CURRENCY_CHOICES)
 
 
     def __str__(self):
